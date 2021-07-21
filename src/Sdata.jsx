1ad5622cc6ae143112efe0cbc81f467249ce716c
Yashvi <yashvigarg330@gmail.com>
2021-07-21 21:36:52 +0530

    done

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -1,12 +1,12 @@
 import tourism from "../src/assets/image/img1.jpg";
 import food from "../src/assets/image/img2.jpg";
-import Images5 from "../src/assets/image/img5.jpg";
+// import Images5 from "../src/assets/image/img5.jpg";
 import Images6 from "../src/assets/image/img6.jpg";
 import Images7 from "../src/assets/image/img7.jpg";
 import Images8 from "../src/assets/image/img8.jpg";
-import Images9 from "../src/assets/image/img9.jpg";
+// import Images9 from "../src/assets/image/img9.jpg";
 import Images10 from "../src/assets/image/img10.jpg";
-import Images11 from "../src/assets/image/img11.jpg";
+// import Images11 from "../src/assets/image/img11.jpg";
 const Sdata = [
   {
     imgsrc: tourism,
